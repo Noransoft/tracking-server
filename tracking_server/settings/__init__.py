bb6a67503afc16970b5b7293ac61945a01b565df
Tapish Kanodia <tapish@noransoft.com>
2017-01-13 12:03:49 +0530

    Configure django

---
+++ tracking_server/settings/__init__.py
@@ -0,0 +1,32 @@
+from tracking_server.settings.common import *  # noqa
+
+
+ALLOWED_HOSTS = ['*']
+
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'  # noqa
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': os.environ['ERROR_LOG_PATH'],  # noqa
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
