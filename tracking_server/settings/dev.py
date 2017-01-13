109513dea97cfc76be6f7c23b33ac161fd7e9577
Tapish Kanodia <tapish@noransoft.com>
2017-01-13 14:47:57 +0530

    Remove static file location

--- tracking_server/settings/dev.py
+++ tracking_server/settings/dev.py
@@ -2,10 +2,3 @@ from tracking_server.settings.common import *  # noqa
 
 
 DEBUG = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # noqa
-]
